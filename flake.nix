03c47dac066f1514db2290251c3da9a1ca77f814
Moritz 'e1mo' Fromm <git@e1mo.de>
2023-01-06 18:29:58 +0100

    services/freescout: Initial setup

--- flake.nix
+++ flake.nix
@@ -5,19 +5,28 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    freescout-nix = {
+      url = "git+https://cyberchaos.dev/e1mo/freescout-nix-flake?ref=main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, sops-nix, flake-utils }: let
+  outputs = inputs@{ self, nixpkgs, sops-nix, flake-utils, freescout-nix }: let
     overlay = import ./packages;
+    allOverlays = [
+      overlay
+      freescout-nix.overlays.default
+    ];
     pkgs = import nixpkgs {
       system = "x86_64-linux";
-      overlays = [ overlay ];
+      overlays = allOverlays;
     };
     defaultModules = [
       sops-nix.nixosModules.sops
+      freescout-nix.nixosModules.freescout
       ./common/default.nix
       {
-        nixpkgs.overlays = [ overlay ];
+        nixpkgs.overlays = allOverlays;
         _module.args = {
           inherit inputs;
           outputs = self;
---
+++ services/freescout.nix
@@ -0,0 +1,27 @@
+{ pkgs, config, baseDomain, ... }:
+
+{
+  sops.secrets."freescout/app_key" = {
+    owner = "freescout";
+    format = "yaml";
+    sopsFile = ../secrets/all/secrets.yaml;
+  };
+
+  services.freescout = {
+    enable = true;
+    domain = "support.${baseDomain}";
+
+    phpPackage = pkgs.php80;
+
+    settings = {
+      APP_KEY._secret  = config.sops.secrets."freescout/app_key".path;
+      APP_ENV = "local";
+      APP_DEBUG = true;
+    };
+    databaseSetup.enable = true;
+    nginx = {
+      forceSSL = true;
+      enableACME = true;
+    };
+  };
+}
