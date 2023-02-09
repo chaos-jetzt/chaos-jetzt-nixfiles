49f72f1b899a4bc286436fd81803a2de713ec04b
Moritz 'e1mo' Fromm <git@e1mo.de>
2023-02-10 00:47:54 +0100

    Add matrix on shirley
    Secrets not yet added to not brick our matrix if we accidentally deploy

--- hosts/shirley/configuration.nix
+++ hosts/shirley/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, baseDomain, ... }: {
+{ pkgs, config, ... }: {
   cj.deployment.environment = "prod";
 
   imports = [
@@ -6,6 +6,7 @@
     ../../services/mumble.nix
     ../../services/website.nix
     ../../services/vaultwarden.nix
+    ../../services/matrix.nix
     ../../services/dokuwiki.nix
     ../../services/freescout.nix
     ../../services/hedgedoc.nix
@@ -27,4 +28,13 @@
       ipv6.addresses = [ { address = "2a01:4f8:c0c:83eb::1"; prefixLength = 64; } ];
     };
   };
+
+  # This is specific to every host!
+  systemd.mounts = [{
+    what = "/dev/disk/by-id/scsi-0HC_Volume_7628580";
+    where = config.services.matrix-synapse.settings.media_store_path;
+    type = "ext4";
+    options = "discard,nofail,defaults";
+    wantedBy = [ "multi-user.target" ];
+  }];
 }
