5722941e1dae6be00cebe9630ed5149b4f9b250b
Moritz 'e1mo' Fromm <git@e1mo.de>
2023-07-28 11:45:39 +0200

    Remove deprevated grub version definition
    Also merged the various network configurations a bit for better clarity

--- hosts/goldberg/hardware-config.nix
+++ hosts/goldberg/hardware-config.nix
@@ -22,7 +22,6 @@
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   # boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
