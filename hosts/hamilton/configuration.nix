f809aafde4284ddc42fbd8a326e5d168644694a7
Moritz 'e1mo' Fromm <git@e1mo.de>
2023-08-13 12:13:50 +0200

    hamilton: Fix network config

--- hosts/hamilton/configuration.nix
+++ hosts/hamilton/configuration.nix
@@ -18,11 +18,11 @@
   networking = {
     # Fallback / for the monitoring v(x)lan
     useDHCP = true;
-    defaultGateway = { address = "172.31.1.1"; interface = "ens3"; };
-    defaultGateway6 = { address = "fe80::1"; interface = "ens3"; };
+    defaultGateway = { address = "172.31.1.1"; interface = "enp1s0"; };
+    defaultGateway6 = { address = "fe80::1"; interface = "enp1s0"; };
     nameservers = [ "213.133.98.98" "213.133.99.99" "213.133.100.100" ];
 
-    interfaces.ens3 = {
+    interfaces.enp1s0 = {
       useDHCP = false;
       ipv4.addresses = [ { address = "128.140.1.30"; prefixLength = 32; } ];
       ipv6.addresses = [ { address = "2a01:4f8:1c1e:b564::1"; prefixLength = 64; } ];
