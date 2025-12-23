a1bd6854ef159830fb5ce9ce1a1e97c79fb98ae6
Anton Mrosek <github@anjomro.de>
2025-12-23 18:50:14 +0100

    Add jetzt12 shortlink

--- services/website.nix
+++ services/website.nix
@@ -50,6 +50,7 @@ in {
         "/theme/fonts/open-sans/fonts/".extraConfig = ''
           expires 1M;
         '';
+        "/12".return = "301 https://chaos.jetzt/articles/jetzt12.html";
         "/jetzt11".return = "301 https://chaos.jetzt/articles/jetzt11.html";
         "/coc".return = "301 https://${baseDomain}/pages/coc.html";
         "/info".return = "301 https://md.chaos.jetzt/jetzt11-uebersicht#";
